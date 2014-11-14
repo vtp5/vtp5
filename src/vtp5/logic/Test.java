bb2d18643ba0d671ffcdfffbcae2a63d59b6b7f8
Nikunj Paliwal <iamnikunjp@gmail.com>
2014-11-14 17:00:28 +0000

    Renamed Card to Test.
    The info we get from the .txt file will be put into the Test object. We
    need to think of a way to allow multiple answers for one question. Also,
    renamed the latin and english arrays to langFrom and langTo
    respectively.

---
+++ src/vtp5/logic/Test.java
@@ -0,0 +1,24 @@
+package vtp5.logic;
+
+public class Test {
+	
+	// TODO Find a way to have multiple answers.
+	
+	private String[] langFrom;
+	private String[] langTo;
+	
+	public String[] getLangFrom() {	
+		return this.langFrom;	}
+	
+	public void setLangFrom(String[] langFrom) {	
+		this.langFrom = langFrom;	
+	}
+	
+	public String[] getLangTo() {	
+		return this.langTo;
+	}
+	
+	public void setLangTo(String[] langTo) {	
+		this.langTo = langTo;
+	}
+}
