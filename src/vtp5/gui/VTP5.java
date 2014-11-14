2e00c6329ea1fe461b55df4c27444b225ef3b2ff
Nikunj Paliwal <iamnikunjp@gmail.com>
2014-11-14 21:59:12 +0000

    Added file reading capabilites

--- src/vtp5/gui/VTP5.java
+++ src/vtp5/gui/VTP5.java
@@ -3,18 +3,27 @@ package vtp5.gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import vtp5.logic.TestFile;
 
 public class VTP5 extends JFrame {
 	// TODO Generate the serialVersionUID once class has been finished.
+	
+	// GUI components.
 	private JMenu menu;
 	private JMenuBar bar;
 	private JMenuItem importItem;
+	private JFileChooser chooser = new JFileChooser();
+	
+	private FileNameExtensionFilter chooserFilter = new FileNameExtensionFilter("Text Files (*.txt)", "txt");
+	
+	TestFile test;
 
 	public VTP5() {
 		// Sets up JMenuBar.
@@ -23,10 +32,14 @@ public class VTP5 extends JFrame {
 		importItem = new JMenuItem("Import");
 		importItem.addActionListener(new MenuItemListener());
 		menu.setMnemonic(KeyEvent.VK_F);
+		importItem.setMnemonic(KeyEvent.VK_I);
 		menu.add(importItem);
 		bar.add(menu);
 		setJMenuBar(bar);
 
+		// Sets up JFileChooser
+		chooser.setFileFilter(chooserFilter);		
+		
 		// Sets JFrame properties.
 		setSize(500, 400);
 		setTitle("VTP5");
@@ -37,9 +50,15 @@ public class VTP5 extends JFrame {
 
 	private class MenuItemListener implements ActionListener {
 		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			if (arg0.getSource() == importItem) {
-				// Will run the open file method(s) here.
+		public void actionPerformed(ActionEvent ae) {
+			if (ae.getSource() == importItem) {
+				// Open JFileChooser and then creates test file
+				int selected = chooser.showOpenDialog(getParent());
+				if (selected == JFileChooser.APPROVE_OPTION) {
+					// Show confirmation dialog if currently in a test.
+					// If so, clear old test.
+					test = new TestFile(chooser.getSelectedFile());
+				}
 			}
 		}
 
--- src/vtp5/logic/TestFile.java
+++ src/vtp5/logic/TestFile.java
@@ -1,11 +1,22 @@
 package vtp5.logic;
 
+import java.io.File;
+
 public class TestFile {
 	
 	// TODO Find a way to have multiple answers.
 	
 	private String[] langFrom;
 	private String[] langTo;
+	
+	public TestFile(File file) {
+		getVocabFromFile(file);
+	}
+	
+	public void getVocabFromFile(File file) {
+		// Something will translate the file to all the possible questions and answers.
+		// System.out.println(file);
+	}
 	
 	public String[] getLangFrom() {	
 		return this.langFrom;	}
