df90229451c727e3e5992ba3e9323d2bac6af1c0
Nikunj Paliwal <iamnikunjp@gmail.com>
2014-11-14 11:57:51 +0000

    Added JFrame

--- src/duckifyz/vtp5/VTP5.java
+++ src/duckifyz/vtp5/VTP5.java
@@ -1,5 +1,19 @@
 package duckifyz.vtp5;
 
-public class VTP5 {
+import javax.swing.JFrame;
 
+public class VTP5 extends JFrame {
+	private static final long serialVersionUID = -5105462185201329709L;
+	
+	public VTP5() {
+		setSize(500, 400);
+		setTitle("VTP5");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new VTP5();
+	}
 }
