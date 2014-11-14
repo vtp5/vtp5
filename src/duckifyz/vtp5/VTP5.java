60202a11a0f91613ed61fc2ba25ad1976da6d357
Yousuf <youmed.tech@gmail.com>
2014-11-14 13:50:10 +0000

    Text Field

--- src/duckifyz/vtp5/VTP5.java
+++ src/duckifyz/vtp5/VTP5.java
@@ -1,9 +1,12 @@
 package duckifyz.vtp5;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class VTP5 extends JFrame {
 	private static final long serialVersionUID = -5105462185201329709L;
+	
+	JTextField field;
 	
 	public VTP5() {
 		setSize(500, 400);
