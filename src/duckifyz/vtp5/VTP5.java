0b59f70ce9e71bb9f53d9d68ba4b40e4fb146f9c
Yousuf <youmed.tech@gmail.com>
2014-11-14 13:58:04 +0000

    Added basic JMenu - Yousuf

--- src/duckifyz/vtp5/VTP5.java
+++ src/duckifyz/vtp5/VTP5.java
@@ -1,14 +1,32 @@
 package duckifyz.vtp5;
 
+import java.awt.event.KeyEvent;
+
 import javax.swing.JFrame;
-import javax.swing.JTextField;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class VTP5 extends JFrame {
 	private static final long serialVersionUID = -5105462185201329709L;
 	
-	JTextField field;
+	private JMenu menu;
+	private JMenuBar bar;
+	private JMenuItem importItem;
+	
 	
 	public VTP5() {
+		bar = new JMenuBar();
+		menu = new JMenu("File");
+		importItem = new JMenuItem("Import");
+		
+		menu.setMnemonic(KeyEvent.VK_F);
+		menu.add(importItem);
+		bar.add(menu);
+		setJMenuBar(bar);
+		
+		
+		
 		setSize(500, 400);
 		setTitle("VTP5");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
