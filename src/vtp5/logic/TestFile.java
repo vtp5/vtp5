b04389c341d552bdf2e9b8f7d6e6fcecbef0ff9f
Nikunj Paliwal <iamnikunjp@gmail.com>
2014-11-14 22:17:46 +0000

    Extended the file-reading capabilities.
    The getVocabFromFile() method now sorts the text into langFrom and
    langTo.

--- src/vtp5/logic/TestFile.java
+++ src/vtp5/logic/TestFile.java
@@ -4,35 +4,47 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TestFile {
 
 	// TODO Find a way to have multiple answers.
 
-	private String[] langFrom;
-	private String[] langTo;
+	private ArrayList<String> langFrom = new ArrayList<String>();
+	private ArrayList<String> langTo = new ArrayList<String>();
 
-	BufferedReader br = null;
+	private BufferedReader br = null;
+	
+	private int switcher = 0;
 
 	public TestFile(File file) {
 		getVocabFromFile(file);
 	}
 
-	public void getVocabFromFile(File file) {
-		// Something will translate the file to all the possible questions and
-		// answers.
+	public void getVocabFromFile(File file) {		
 		System.out.println(file + " is being read.");
 
 		try {
-			String sCurrentLine;
+			// Reading file.
+			String currentLine;
 			br = new BufferedReader(new FileReader(file));
-			while ((sCurrentLine = br.readLine()) != null) {
-				System.out.println(sCurrentLine);
+			while ((currentLine = br.readLine()) != null) {
+				// If switcher = 0 (default) add to langFrom.
+				// If switcher = 1, add to langTo.
+				if (switcher == 0) {
+					langFrom.add(currentLine);
+					switcher = 1;
+				} else if (switcher == 1) {
+					langTo.add(currentLine);
+					switcher = 0;
+				}
 			}
+			// Catch any excpetions.
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
+				// Close reader.
 				if (br != null) {
 					br.close();
 				}
@@ -40,21 +52,26 @@ public class TestFile {
 				ex.printStackTrace();
 			}
 		}
+		System.out.println(langFrom);
+		System.out.println(langTo);
 	}
-
-	public String[] getLangFrom() {
+	
+	public ArrayList<String> getLangFrom() {	
 		return this.langFrom;
 	}
 
-	public void setLangFrom(String[] langFrom) {
+	
+	public void setLangFrom(ArrayList<String> langFrom) {	
 		this.langFrom = langFrom;
 	}
 
-	public String[] getLangTo() {
+	
+	public ArrayList<String> getLangTo() {	
 		return this.langTo;
 	}
 
-	public void setLangTo(String[] langTo) {
+	
+	public void setLangTo(ArrayList<String> langTo) {	
 		this.langTo = langTo;
 	}
 }
