483a6c2542ae9df284f579c703dc0497b4af47d5
Nikunj Paliwal <iamnikunjp@gmail.com>
2014-11-14 17:01:23 +0000

    Formatted and commented VTP5.java and removed reduntant Card.java.

--- src/vtp5/gui/VTP5.java
+++ src/vtp5/gui/VTP5.java
@@ -11,23 +11,23 @@ import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
 
 public class VTP5 extends JFrame {
-	// TODO Generate the serialVersionUID once class has been finished
-	// private static final long serialVersionUID = -5105462185201329709L;
+	// TODO Generate the serialVersionUID once class has been finished.
 	private JMenu menu;
 	private JMenuBar bar;
 	private JMenuItem importItem;
 
 	public VTP5() {
+		// Sets up JMenuBar.
 		bar = new JMenuBar();
 		menu = new JMenu("File");
 		importItem = new JMenuItem("Import");
 		importItem.addActionListener(new MenuItemListener());
-
 		menu.setMnemonic(KeyEvent.VK_F);
 		menu.add(importItem);
 		bar.add(menu);
 		setJMenuBar(bar);
 
+		// Sets JFrame properties.
 		setSize(500, 400);
 		setTitle("VTP5");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -36,20 +36,17 @@ public class VTP5 extends JFrame {
 	}
 
 	private class MenuItemListener implements ActionListener {
-
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			if (arg0.getSource() == importItem) {
-				// STUFF
+				// Will run the open file method(s) here.
 			}
 		}
 
 	}
 
 	public static void main(String[] args) {
-		// TODO CHANGED
 		SwingUtilities.invokeLater(new Runnable() {
-
 			@Override
 			public void run() {
 				new VTP5();
