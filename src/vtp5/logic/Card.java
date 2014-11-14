5a7dfc723184c15b9969d2ddd3d0618fae2898f1
leMingathon <smartsir123@gmail.com>
2014-11-14 15:17:16 +0000

    Added Card.java

---
+++ src/vtp5/logic/Card.java
@@ -0,0 +1,22 @@
+package vtp5.logic;
+
+public class Card {
+	private String[] latin;
+	private String[] english;
+
+	public String[] getLatin() {
+		return latin;
+	}
+
+	public void setLatin(String[] latin) {
+		this.latin = latin;
+	}
+
+	public String[] getEnglish() {
+		return english;
+	}
+
+	public void setEnglish(String[] english) {
+		this.english = english;
+	}
+}
