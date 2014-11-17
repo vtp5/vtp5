55f5fce8936b2f23b4b096a2e323e15ad30ce47d
Le <Le@awesome_pc>
2014-11-17 20:56:02 +0000

    Commented out menu bar code, added "menu button" to GUI

--- src/vtp5/gui/VTP5.java
+++ src/vtp5/gui/VTP5.java
@@ -1,25 +1,33 @@
 package vtp5.gui;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileNameExtensionFilter;
+
+import net.miginfocom.swing.MigLayout;
 import vtp5.logic.TestFile;
 
 public class VTP5 extends JFrame {
 	// TODO Generate the serialVersionUID once class has been finished.
 
 	// GUI components.
-	private JMenu newTestMenu, importFilesMenu, recordsMenu, helpMenu,
-			settingsMenu;
-	private JMenuItem importText;
-	private JMenuBar bar;
+	// private JMenu newTestMenu, importFilesMenu, recordsMenu, helpMenu,
+	// settingsMenu;
+	// private JMenuItem importText;
+	// private JMenuBar bar;
+
+	// Components for button panel at top of frame
+	private JPanel buttonPanel;
+	private JButton importFileButton, leaderboardButton, settingsButton,
+			helpButton;
+
+	// Components in the main area of the frame
+	private JPanel mainPanel;
 
 	private JFileChooser chooser = new JFileChooser();
 
@@ -30,34 +38,51 @@ public class VTP5 extends JFrame {
 
 	public VTP5() {
 		// Sets up JMenuBar.
-		bar = new JMenuBar();
-		newTestMenu = new JMenu("New Test");
-		newTestMenu.setMnemonic(KeyEvent.VK_N);
-
-		importFilesMenu = new JMenu("Import");
-		importFilesMenu.setMnemonic(KeyEvent.VK_I);
-		importText = new JMenuItem("Text File");
-		importText.addActionListener(new MenuItemListener());
-		importFilesMenu.add(importText);
-
-		recordsMenu = new JMenu("Records");
-		recordsMenu.setMnemonic(KeyEvent.VK_R);
-
-		helpMenu = new JMenu("Help");
-		helpMenu.setMnemonic(KeyEvent.VK_H);
-
-		settingsMenu = new JMenu("Settings");
-		settingsMenu.setMnemonic(KeyEvent.VK_S);
-
-		bar.add(newTestMenu);
-		bar.add(importFilesMenu);
-		bar.add(recordsMenu);
-		bar.add(helpMenu);
-		bar.add(settingsMenu);
-		setJMenuBar(bar);
+		// bar = new JMenuBar();
+		// newTestMenu = new JMenu("New Test");
+		// newTestMenu.setMnemonic(KeyEvent.VK_N);
+		//
+		// importFilesMenu = new JMenu("Import");
+		// importFilesMenu.setMnemonic(KeyEvent.VK_I);
+		// importText = new JMenuItem("Text File");
+		// importText.addActionListener(new MenuItemListener());
+		// importFilesMenu.add(importText);
+		//
+		// recordsMenu = new JMenu("Records");
+		// recordsMenu.setMnemonic(KeyEvent.VK_R);
+		//
+		// helpMenu = new JMenu("Help");
+		// helpMenu.setMnemonic(KeyEvent.VK_H);
+		//
+		// settingsMenu = new JMenu("Settings");
+		// settingsMenu.setMnemonic(KeyEvent.VK_S);
+		//
+		// bar.add(newTestMenu);
+		// bar.add(importFilesMenu);
+		// bar.add(recordsMenu);
+		// bar.add(helpMenu);
+		// bar.add(settingsMenu);
+		// setJMenuBar(bar);
 
 		// Sets up JFileChooser
 		chooser.setFileFilter(chooserFilter);
+
+		// Set up button panel
+		buttonPanel = new JPanel();
+		buttonPanel.setLayout(new MigLayout());
+
+		importFileButton = new JButton("Import Test File");
+		leaderboardButton = new JButton("View Leaderboards");
+		settingsButton = new JButton("Settings");
+		helpButton = new JButton("Help");
+
+		buttonPanel.add(importFileButton, "align left");
+		buttonPanel.add(leaderboardButton, "push, align right");
+		buttonPanel.add(settingsButton, "align right");
+		buttonPanel.add(helpButton, "align right");
+
+		// Add panels to JFrame
+		getContentPane().add(buttonPanel, BorderLayout.NORTH);
 
 		// Sets JFrame properties.
 		setSize(500, 400);
@@ -67,21 +92,21 @@ public class VTP5 extends JFrame {
 		setVisible(true);
 	}
 
-	private class MenuItemListener implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent ae) {
-			if (ae.getSource() == importText) {
-				// Open JFileChooser and then creates test file
-				int selected = chooser.showOpenDialog(getParent());
-				if (selected == JFileChooser.APPROVE_OPTION) {
-					// Show confirmation dialog if currently in a test.
-					// If so, clear old test.
-					test = new TestFile(chooser.getSelectedFile());
-				}
-			}
-		}
-
-	}
+	// private class MenuItemListener implements ActionListener {
+	// @Override
+	// public void actionPerformed(ActionEvent ae) {
+	// if (ae.getSource() == importText) {
+	// // Open JFileChooser and then creates test file
+	// int selected = chooser.showOpenDialog(getParent());
+	// if (selected == JFileChooser.APPROVE_OPTION) {
+	// // Show confirmation dialog if currently in a test.
+	// // If so, clear old test.
+	// test = new TestFile(chooser.getSelectedFile());
+	// }
+	// }
+	// }
+	//
+	// }
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
