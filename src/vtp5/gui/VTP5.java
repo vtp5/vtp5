508b126ab5323182c2741fc93c365df54c080b80
Le <Le@awesome_pc>
2014-11-17 21:36:47 +0000

    Added all components to GUI (although doesn't look very good as yet)

--- src/vtp5/gui/VTP5.java
+++ src/vtp5/gui/VTP5.java
@@ -5,7 +5,12 @@ import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -24,10 +29,19 @@ public class VTP5 extends JFrame {
 	// Components for button panel at top of frame
 	private JPanel buttonPanel;
 	private JButton importFileButton, leaderboardButton, settingsButton,
-			helpButton;
+			helpButton, aboutButton;
 
 	// Components in the main area of the frame
 	private JPanel mainPanel;
+	private JLabel promptLabel;
+	private JTextField answerField;
+	private JButton enterButton;
+	private JButton passButton;
+	private JList<String> statsList;
+	private JScrollPane statsScrollPane;
+	private JList<String> guessedAnswersList;
+	private JScrollPane guessedAnswersScrollPane;
+	private JProgressBar progressBar;
 
 	private JFileChooser chooser = new JFileChooser();
 
@@ -75,17 +89,52 @@ public class VTP5 extends JFrame {
 		leaderboardButton = new JButton("View Leaderboards");
 		settingsButton = new JButton("Settings");
 		helpButton = new JButton("Help");
+		aboutButton = new JButton("About");
 
 		buttonPanel.add(importFileButton, "align left");
 		buttonPanel.add(leaderboardButton, "push, align right");
 		buttonPanel.add(settingsButton, "align right");
 		buttonPanel.add(helpButton, "align right");
+		buttonPanel.add(aboutButton, "align right");
+
+		// Set up main panel
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new MigLayout("insets 5", "", ""));
+
+		promptLabel = new JLabel(
+				"<html><div style=\"text-align:center;\">Prompt</div></html>");
+		answerField = new JTextField("Enter answer here");
+		enterButton = new JButton("Enter");
+		passButton = new JButton("Pass");
+
+		// Set up JLists and their respective ListModels
+		statsList = new JList<>(new String[] { "Stats:", "1", "2", "3", "4" });
+		statsList.setVisibleRowCount(5);
+		statsScrollPane = new JScrollPane(statsList);
+		guessedAnswersList = new JList<>(new String[] {
+				"Already guessed answers:", "1", "2", "3", "4" });
+		guessedAnswersList.setVisibleRowCount(5);
+		guessedAnswersScrollPane = new JScrollPane(guessedAnswersList);
+
+		progressBar = new JProgressBar(JProgressBar.VERTICAL, 0, 100);
+		progressBar.setValue(50);
+
+		// Add components to main panel
+		mainPanel.add(promptLabel, "span 3, push, wrap");
+		mainPanel.add(answerField, "span 2 2, grow");
+		mainPanel.add(enterButton, "wrap");
+		mainPanel.add(passButton, "wrap");
+		mainPanel.add(statsScrollPane, "grow");
+		mainPanel.add(guessedAnswersScrollPane, "grow, push, span");
+
+		mainPanel.add(progressBar, "dock east");
 
 		// Add panels to JFrame
 		getContentPane().add(buttonPanel, BorderLayout.NORTH);
+		getContentPane().add(mainPanel, BorderLayout.CENTER);
 
 		// Sets JFrame properties.
-		setSize(500, 400);
+		setSize(700, 600);
 		setTitle("VTP5");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
