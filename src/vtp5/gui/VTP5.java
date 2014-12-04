d647edd4d46fe3f891e09ab525a93adafb68a39f
Yousuf <youmed.tech@gmail.com>
2014-12-04 11:54:58 +0000

    Moved all the font setting to a separate class

--- src/vtp5/gui/VTP5.java
+++ src/vtp5/gui/VTP5.java
@@ -376,30 +376,31 @@ public class VTP5 extends JFrame {
 
 	// Method that returns a font object with the "default" font family
 	private void setFontSize(Component c, int fontSize) {
+		CustomFont cf = new CustomFont();
+		cf.setFont(c, fontSize);
+		// try {
+		/*
+		 * Font font = Font.createFont(Font.TRUETYPE_FONT, new
+		 * FileInputStream("res/fonts/ubuntu/Ubuntu-C.ttf")); // font =
+		 * Font.createFont(Font.TRUETYPE_FONT, Loader //
+		 * .getInputStream("fonts/didactgothic/DidactGothic.ttf")); /* Font
+		 * font1 = new Font.createFont(Font.TRUETYPE_FONT,
+		 * (FileInputStream)Loader.getInputStream(
+		 * "res/fonts/didactgothic/DidactGothic.ttf"));
+		 */
+		// font = font.deriveFont((float) fontSize);
+		// GraphicsEnvironment.getLocalGraphicsEnvironment()
+		// .registerFont(font);
+		// c.setFont(font);
 
-		try {
-			Font font = Font.createFont(Font.TRUETYPE_FONT,
-					new FileInputStream("res/fonts/ubuntu/Ubuntu-C.ttf"));
-			// font = Font.createFont(Font.TRUETYPE_FONT, Loader
-			// .getInputStream("fonts/didactgothic/DidactGothic.ttf"));
-			/*
-			 * Font font1 = new Font.createFont(Font.TRUETYPE_FONT,
-			 * (FileInputStream)Loader.getInputStream(
-			 * "res/fonts/didactgothic/DidactGothic.ttf"));
-			 */
-			font = font.deriveFont((float) fontSize);
-			GraphicsEnvironment.getLocalGraphicsEnvironment()
-					.registerFont(font);
-			c.setFont(font);
+		// } catch (FontFormatException | IOException e) {
+		// JOptionPane.showMessageDialog(this, "The font file was not found.",
+		// "VTP5", JOptionPane.ERROR_MESSAGE);
+		// e.printStackTrace();
 
-		} catch (FontFormatException | IOException e) {
-			JOptionPane.showMessageDialog(this, "The font file was not found.",
-					"VTP5", JOptionPane.ERROR_MESSAGE);
-			e.printStackTrace();
-
-			// Use Arial font (because pretty much everyone has it)
-			new Font("Arial", Font.PLAIN, fontSize);
-		}
+		// Use Arial font (because pretty much everyone has it)
+		// new Font("Arial", Font.PLAIN, fontSize);
+		// }
 	}
 
 	private void showChooserDialog(int fileType) {
