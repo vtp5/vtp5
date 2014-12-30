1fc8cbc21d6bed076c0bb7644cad0b85a49b900a
abdelabdalla <abdelabdalla@gmail.com>
2014-12-30 21:06:26 +0000

    Getting closer to a usable launcher

--- src/vtp_launcher/Initial.java
+++ src/vtp_launcher/Initial.java
@@ -1,7 +1,11 @@
 package vtp_launcher;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -18,7 +22,7 @@ public class Initial extends Launcher{
 		
 		tp.setText(p[0]);
 		
-		String vtpdir = p[0]+"\\VTP5_files";
+		String vtpdir = p[0]+"\\VTP5_files\\vtp5";
 		
 		File theDir = new File(vtpdir);
 
@@ -35,9 +39,25 @@ public class Initial extends Launcher{
 		     }        
 		     if(result) {    
 		       System.out.println("DIR created");  
+		       
 		     }
 		  }else{
 			  System.out.println("Already there");
+			  
+			  File fi = new File(vtpdir + "\\" + vtpversion + ".jar");
+			  if(fi.exists()) {
+				  System.out.println("Latest build present");
+			  }
+			  else{
+				  
+				  File srcFolder = new File("http://192.168.1.141/vtp5"  );
+				  try {
+					copyFolder(srcFolder,theDir);
+				} catch (IOException e) {
+					
+					e.printStackTrace();
+				}
+			  }
 			  
 		  }
 		
@@ -60,4 +80,46 @@ public class Initial extends Launcher{
 		
 	}
 	
+	public static void copyFolder(File src, File dest)
+	    	throws IOException{
+	 
+	    	if(src.isDirectory()){
+	 
+	    		//if directory not exists, create it
+	    		if(!dest.exists()){
+	    		   dest.mkdir();
+	    		   System.out.println("Directory copied from " 
+	                              + src + "  to " + dest);
+	    		}
+	 
+	    		//list all the directory contents
+	    		String files[] = src.list();
+	 
+	    		for (String file : files) {
+	    		   //construct the src and dest file structure
+	    		   File srcFile = new File(src, file);
+	    		   File destFile = new File(dest, file);
+	    		   //recursive copy
+	    		   copyFolder(srcFile,destFile);
+	    		}
+	 
+	    	}else{
+	    		//if file, then copy it
+	    		//Use bytes stream to support all file types
+	    		InputStream in = new FileInputStream(src);
+	    	        OutputStream out = new FileOutputStream(dest); 
+	 
+	    	        byte[] buffer = new byte[1024];
+	 
+	    	        int length;
+	    	        //copy the file content in bytes 
+	    	        while ((length = in.read(buffer)) > 0){
+	    	    	   out.write(buffer, 0, length);
+	    	        }
+	 
+	    	        in.close();
+	    	        out.close();
+	    	        System.out.println("File copied from " + src + " to " + dest);
+	    	}
+	    }
 }
