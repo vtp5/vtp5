92e6bb01ed7713369fce7dd9df65b88833f3aa02
leMingathon <smartsir123@gmail.com>
2015-01-15 14:17:59 +0000

    Disabled selection for all JLists

--- src/vtp5/gui/VTP5.java
+++ src/vtp5/gui/VTP5.java
@@ -1034,6 +1034,11 @@ public class VTP5 extends JFrame {
 
 	private class DisabledItemSelectionModel extends DefaultListSelectionModel {
 
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
 		@Override
 		public void setSelectionInterval(int index0, int index1) {
 			super.setSelectionInterval(-1, -1);
