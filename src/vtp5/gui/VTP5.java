ed0f030ef4328f454c50b9454479c2a059a72152
Nikunj Paliwal <iamnikunjp@gmail.com>
2015-01-17 17:21:03 +0000

    Fixed a small bug: when resetting to defaults, the panel would go white until the user clicked enter

--- src/vtp5/gui/VTP5.java
+++ src/vtp5/gui/VTP5.java
@@ -1065,7 +1065,7 @@ public class VTP5 extends JFrame {
 		buttonColour = Color.BLACK;
 		buttonTextColor = Color.WHITE;
 		textColour = Color.BLACK;
-		panelColour = null;
+		calculateFrameColour(test.getStats());
 		questionNumberCheck.setSelected(true);
 		experimentalCheck.setSelected(true);
 		setColour(buttonColour, buttonTextColor, textColour);
