d62bff69cccec852fa1afc9ab178c8121bdba6c1
Abdel Abdalla <abdelabdalla@gmail.com>
2015-01-27 18:40:27 +0000

    Added update search at start
    It is in a thread by itself so it shouldn't slow down the launch.
    Initial tests show that it doesn't slow the program down

--- src/vtp5/Main.java
+++ src/vtp5/Main.java
@@ -1,8 +1,19 @@
 package vtp5;
 
+import java.awt.Desktop;
+import java.awt.Font;
+import java.io.IOException;
+
+import javax.swing.JEditorPane;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+
+import org.kohsuke.github.GHRelease;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
 import com.swabunga.spell.engine.EditDistance;
 
@@ -34,6 +45,11 @@ public class Main {
 	public static final boolean exportingToJar = true;
 
 	public static void main(String[] args) {
+		
+		Runnable r = new Update();
+        Thread t = new Thread(r);
+        t.start();
+        
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -60,4 +76,63 @@ public class Main {
 			}
 		});
 	}
+}
+
+
+class Update implements Runnable{
+    public void run(){
+        //TODO search for latest update
+    	System.out.println("Searching for new update");
+    	
+    	try {
+			GHRepository repo = GitHub.connectAnonymously().getRepository(
+					"vtp5/vtp5");
+			GHRelease release = repo.listReleases().asList().get(0);
+
+			if (release.getTagName().contains(Main.build)
+					|| release.getName().contains(Main.build)) {
+			//why do anything
+			} else {
+				JEditorPane editorPane = new JEditorPane();
+				editorPane.setContentType("text/html");
+				//editorPane.setFontSize(20);
+				//editorPane.setFont(new Font("Segoe Script", 0, 20));
+				editorPane
+						.setText("You are not running the latest release of VTP5. \n\nThe latest version, "
+								+ release.getName()
+								+ ", can be found "
+								+ "<a href='https://github.com/vtp5/vtp5/releases'>here</a>.");
+
+				editorPane.setEditable(false);
+				editorPane.setOpaque(false);
+
+				editorPane.addHyperlinkListener(new HyperlinkListener() {
+					@Override
+					public void hyperlinkUpdate(HyperlinkEvent hle) {
+						if (HyperlinkEvent.EventType.ACTIVATED.equals(hle
+								.getEventType())) {
+							System.out.println(hle.getURL());
+							Desktop desktop = Desktop.getDesktop();
+							try {
+								desktop.browse(hle.getURL().toURI());
+							} catch (Exception ex) {
+								ex.printStackTrace();
+							}
+						}
+					}
+				});
+
+				JOptionPane.showMessageDialog(editorPane, "VTP5");
+			}
+		} catch (IOException e) {
+			JOptionPane
+					.showMessageDialog(
+							null,
+							"The following error occurred:\n\n"
+									+ e.toString()
+									+ "\n\nYour computer probably isn't connected to the Internet.",
+							"VTP5", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+    }
 }
