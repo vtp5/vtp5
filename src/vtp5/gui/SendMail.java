b2a86f1beaa1b56d64760179107089758e062e35
leMingathon <smartsir123@gmail.com>
2015-01-31 19:20:14 +0000

    Added a TODO to add exception-handling code

--- src/vtp5/gui/SendMail.java
+++ src/vtp5/gui/SendMail.java
@@ -28,57 +28,57 @@ import vtp5.logic.TestFile;
 
 /*VTP5 Copyright (C) 2015  Abdel-Rahim Abdalla, Minghua Yin, Yousuf Mohamed-Ahmed and Nikunj Paliwal
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 public class SendMail {
-	
-	static String u1,u2,u3;
+
+	static String u1, u2, u3;
 
 	@SuppressWarnings("deprecation")
 	public static void m() throws IOException {
-	      JTextField uField = new JTextField(10);
-	      JPasswordField pField = new JPasswordField(10);
-	      JTextField tField = new JTextField(10);
+		JTextField uField = new JTextField(10);
+		JPasswordField pField = new JPasswordField(10);
+		JTextField tField = new JTextField(10);
 
-	      JPanel myPanel = new JPanel();
-	      myPanel.add(new JLabel("Email:"));
-	      myPanel.add(uField);
-	      myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-	      myPanel.add(new JLabel("Password:"));
-	      myPanel.add(pField);
-	      myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-	      myPanel.add(new JLabel("Send To:"));
-	      myPanel.add(tField);
+		JPanel myPanel = new JPanel();
+		myPanel.add(new JLabel("Email:"));
+		myPanel.add(uField);
+		myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+		myPanel.add(new JLabel("Password:"));
+		myPanel.add(pField);
+		myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+		myPanel.add(new JLabel("Send To:"));
+		myPanel.add(tField);
 
-	      int result = JOptionPane.showConfirmDialog(null, myPanel, 
-	               "Sign In", JOptionPane.OK_CANCEL_OPTION);
-	      if (result == JOptionPane.OK_OPTION) {
-	    	  u1 = uField.getText();
-	    	  u2 = pField.getText();
-	    	  u3 = tField.getText();
-	         System.out.println("u value: " + uField.getText());
-	         //System.out.println("y value: " + yField.getText());
-	         if(u1.contains("@reading-school.co.uk")){
-	         mail();
-	         }else{
-	        	 System.out.println("not reading school");
-	         }
-	         
-	      }
+		int result = JOptionPane.showConfirmDialog(null, myPanel, "Sign In",
+				JOptionPane.OK_CANCEL_OPTION);
+		if (result == JOptionPane.OK_OPTION) {
+			u1 = uField.getText();
+			u2 = pField.getText();
+			u3 = tField.getText();
+			System.out.println("u value: " + uField.getText());
+			// System.out.println("y value: " + yField.getText());
+			if (u1.contains("@reading-school.co.uk")) {
+				mail();
+			} else {
+				System.out.println("not reading school");
+			}
+
+		}
 	}
-	
+
 	public static void mail() throws IOException {
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
@@ -97,33 +97,34 @@ public class SendMail {
 			message.setRecipients(Message.RecipientType.TO,
 					InternetAddress.parse(u3));
 			message.setSubject("VTP5 Screenshot");
-			//message.setText("Body of the email");
-			 // Create the message part 
-	         BodyPart messageBodyPart = new MimeBodyPart();
+			// message.setText("Body of the email");
+			// Create the message part
+			BodyPart messageBodyPart = new MimeBodyPart();
 
-	         // Fill the message
-	         
-	         messageBodyPart.setText("Score: "+TestFile.score);
-	         
-	         // Create a multipar message
-	         Multipart multipart = new MimeMultipart();
+			// Fill the message
 
-	         // Set text message part
-	         multipart.addBodyPart(messageBodyPart);
+			messageBodyPart.setText("Score: " + TestFile.score);
 
-	         // Part two is attachment
-	         messageBodyPart = new MimeBodyPart();
-	         String filename = FinishPanel.screenloc;
-	         DataSource source = new FileDataSource(filename);
-	         messageBodyPart.setDataHandler(new DataHandler(source));
-	         messageBodyPart.setFileName(filename);
-	         multipart.addBodyPart(messageBodyPart);
+			// Create a multipar message
+			Multipart multipart = new MimeMultipart();
 
-	         // Send the complete message parts
-	         message.setContent(multipart);
+			// Set text message part
+			multipart.addBodyPart(messageBodyPart);
+
+			// Part two is attachment
+			messageBodyPart = new MimeBodyPart();
+			String filename = FinishPanel.screenloc;
+			DataSource source = new FileDataSource(filename);
+			messageBodyPart.setDataHandler(new DataHandler(source));
+			messageBodyPart.setFileName(filename);
+			multipart.addBodyPart(messageBodyPart);
+
+			// Send the complete message parts
+			message.setContent(multipart);
 			Transport.send(message);
 			System.out.println("Sent");
 		} catch (MessagingException e) {
+			// TODO EXCEPTION-HANDLING CODE!
 			e.printStackTrace();
 		}
 	}
